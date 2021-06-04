70404d163254be76df965574015fbfc31efae014
kabzangwana <79355407+kabzangwana@users.noreply.github.com>
2021-06-04 16:46:30 +0200

    Update

--- server.js
+++ server.js
@@ -2,20 +2,26 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const port = 3000;
-
+const Router = require('router');
 const uri = 'mongodb://localhost:27017/KabzaUniversityDB';
+
+const router = Router();
 
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     serverSelectionTimeoutMS: 5000,
  }).then(() => {
-       console.log('Database sucessfully connected')
+       console.log('Database successfully connected')
     },
     error => {
        console.log('Database could not connected: ' + error)
     }
  )
+
+app.get('/', function (req, res) {
+  res.send('hello world');
+});
 
 app.listen(port, () => {
     console.log(`Application listening on port ${port}`);
